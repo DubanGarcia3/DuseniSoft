f82d966b7d520b289e8b9eae77f112ea951367a6
Duban <mr.garcialml@gmail.com>
2019-11-03 22:45:33 -0500

    welcome

--- DuseniSoft/src/app/app-routing.module.ts
+++ DuseniSoft/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './Components/welcome/welcome.component';
+import { LoginComponent } from './Components/login/login.component';
 
 
 
 const routes: Routes = [
+  {path:'app-welcome', component:WelcomeComponent},
+  {path:'app-login', component:LoginComponent}
 ];
 
 @NgModule({
--- DuseniSoft/src/app/app.component.ts
+++ DuseniSoft/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DuseniSoft';
+
+  constructor(private router:Router){
+    this.welcome();
+  }
+
+  welcome(){
+    this.router.navigate(["app-welcome"]);
+  }
+
+  login(){
+    this.router.navigate(["app-login"]);
+  }
+ 
 }
