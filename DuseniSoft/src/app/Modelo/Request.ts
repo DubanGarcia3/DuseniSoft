0a764ec7de83434f7cc9ef54d3ccc93cbfe855c0
Sebas320 <cbastian320@gmail.com>
2019-11-20 17:12:29 -0500

    Fix minor errors

--- DuseniSoft/src/app/Modelo/Request.ts
+++ DuseniSoft/src/app/Modelo/Request.ts
@@ -2,5 +2,7 @@ export class Request{
     idPedido: number;
     quantity: number;
     dateOrder:Date;
-    dateLimmit: Date;     
+    dateLimmit: Date;
+    unitPrice: number;
+    isActive:boolean;      
 }
