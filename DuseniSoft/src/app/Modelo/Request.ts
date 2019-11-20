327d29c338db4196ccf62e28f2563ebf88a30b3d
Sebas320 <cbastian320@gmail.com>
2019-11-20 17:06:56 -0500

    Se borraron dos atributos del Request que no se necesitan

--- DuseniSoft/src/app/Modelo/Request.ts
+++ DuseniSoft/src/app/Modelo/Request.ts
@@ -2,9 +2,5 @@ export class Request{
     idPedido: number;
     quantity: number;
     dateOrder:Date;
-    dateLimmit: Date;
-    unitPrice: number;
-    isActive:boolean;
-
-      
+    dateLimmit: Date;     
 }
