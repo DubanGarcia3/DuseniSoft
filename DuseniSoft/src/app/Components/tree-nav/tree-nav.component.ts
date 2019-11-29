adf00a182d0e099a0d6f8db45a0d45b796686e39
Duban <mr.garcialml@gmail.com>
2019-11-29 15:02:05 -0500

    arreglo iconos buscar
    iconos y lo del arbol de navegacion

--- DuseniSoft/src/app/Components/tree-nav/tree-nav.component.ts
+++ DuseniSoft/src/app/Components/tree-nav/tree-nav.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatTreeModule} from '@angular/material/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-tree-nav',
