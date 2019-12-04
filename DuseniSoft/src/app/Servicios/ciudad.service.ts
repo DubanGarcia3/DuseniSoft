908676f9f46db084e9a0a411318ea92efa77d6b3
Duban <mr.garcialml@gmail.com>
2019-12-04 16:48:49 -0500

    services de ciudad

---
+++ DuseniSoft/src/app/Servicios/ciudad.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { City } from '../Modelo/City';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CiudadService {
+
+  Url = 'http://localhost:8081/';
+
+  constructor(private http:HttpClient) { }
+
+  getCiudades() {
+    return this.http.get<City[]>(this.Url+"allMembers");
+  }
+
+}
