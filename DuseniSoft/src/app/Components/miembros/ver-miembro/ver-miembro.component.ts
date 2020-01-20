a4b8fbc5f6679538c9a672afe4b96db614419335
Sebas320 <cbastian320@gmail.com>
2020-01-20 11:46:14 -0500

    Bug fixes

--- DuseniSoft/src/app/Components/miembros/ver-miembro/ver-miembro.component.ts
+++ DuseniSoft/src/app/Components/miembros/ver-miembro/ver-miembro.component.ts
@@ -12,15 +12,7 @@ export class VerMiembroComponent implements OnInit {
   miembro : Member;
 
   constructor(
-    private router: Router,
-
-    ) {
-
-    const navigation = this.router.getCurrentNavigation();
-    const state = navigation.extras.state as {example: Member};
-    this.miembro = state.example;
-    
-   }
+    private router: Router) { }
 
   ngOnInit() {
   }
