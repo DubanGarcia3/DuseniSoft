3268e79555ecbdc7272f8f994d1b8b977d5e113a
Sebas320 <cbastian320@gmail.com>
2020-02-05 14:24:51 -0500

    Pequeños cambios

--- DuseniSoft/src/app/Components/productos/add-producto/add-producto.component.ts
+++ DuseniSoft/src/app/Components/productos/add-producto/add-producto.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/Modelo/Product';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductosService } from 'src/app/Servicios/productos.service';
+import { AsociacionesService } from 'src/app/Servicios/asociaciones.service';
+import { PedidosComponent } from '../../pedidos/pedidos.component';
 
 @Component({
   selector: 'app-add-producto',
@@ -11,9 +14,24 @@ export class AddProductoComponent implements OnInit {
 
   productoAAgregar: Product = new Product();
   datos_producto_formulario: FormGroup;
-  constructor() { }
+
+  constructor(
+    private productosService: ProductosService,
+    private asociacionesService: AsociacionesService,
+    private pedidosComponent: PedidosComponent,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.datos_producto_formulario = this.formBuilder.group({
+      //[Valor inicial del campo, Validadores síncronos, Validadores asíncronos]
+      nombre_producto: ['', Validators.required],
+      foto_producto: ['', Validators.required]
+    });
+
+  }
+
+  get formulario(){
+    return this.datos_producto_formulario.controls; 
   }
 
 }
