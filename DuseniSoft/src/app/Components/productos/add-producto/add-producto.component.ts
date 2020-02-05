381a2cfc291ab6a68d51c800e1d02db887b31672
Sebas320 <cbastian320@gmail.com>
2020-02-05 15:55:09 -0500

    Cambios en addProduct

--- DuseniSoft/src/app/Components/productos/add-producto/add-producto.component.ts
+++ DuseniSoft/src/app/Components/productos/add-producto/add-producto.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductosService } from 'src/app/Servicios/productos.service';
 import { AsociacionesService } from 'src/app/Servicios/asociaciones.service';
 import { PedidosComponent } from '../../pedidos/pedidos.component';
+import { Association } from 'src/app/Modelo/Association';
 
 @Component({
   selector: 'app-add-producto',
@@ -18,7 +19,6 @@ export class AddProductoComponent implements OnInit {
   constructor(
     private productosService: ProductosService,
     private asociacionesService: AsociacionesService,
-    private pedidosComponent: PedidosComponent,
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -34,4 +34,24 @@ export class AddProductoComponent implements OnInit {
     return this.datos_producto_formulario.controls; 
   }
 
+  addProduct(){
+    console.log('DATOS DEL PRODUCTO CREADO');
+    console.log('ID '+ this.productoAAgregar.id_product);
+    console.log('NOMBRE '+ this.productoAAgregar.name_product);
+    console.log('IMAGEN '+ this.productoAAgregar.producto_image);
+    //326533 id asociacion
+    this.productoAAgregar.association = new Association();
+    this.asociacionesService.getAsociacionId(326533).subscribe(data => {
+      this.productoAAgregar.association = data;
+    });
+    console.log('ASOCIACIÓN ID '+ this.productoAAgregar.association.id_association);
+  }
+
+  vaciarCampos(){
+    this.datos_producto_formulario = this.formBuilder.group({
+      //[Valor inicial del campo, Validadores síncronos, Validadores asíncronos]
+      nombre_producto: ['', Validators.required],
+      foto_producto: ['', Validators.required]
+    });
+  }
 }
